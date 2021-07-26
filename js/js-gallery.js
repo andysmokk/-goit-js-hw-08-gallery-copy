e674a5c350370ba1c3f3748535790cd94bcec05b
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-07-26 15:11:16 +0200

    +

--- js/js-gallery.js
+++ js/js-gallery.js
@@ -1,21 +1,16 @@
 import galleryItems from './app.js';
 
-const imagesContainerRef = document.querySelector('.js-gallery');
-const lightboxRef = document.querySelector('.lightbox');
-const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
-const lightboxContentRef = document.querySelector('.lightbox__content');
-const lightboxImageRef = document.querySelector('.lightbox__image');
-const lightboxButtonRef = document.querySelector('[data-action="close-lightbox"]');
+const galleryRef = document.querySelector('.js-gallery');
+const lightboxRef = document.querySelector('.js-lightbox');
+const lightboxImagRef = document.querySelector('.lightbox__image');
+const btnClsRef = document.querySelector('[data-action="close-lightbox"]');
+const overlayRef = document.querySelector('.lightbox__overlay');
 
-console.log(lightboxImageRef)
+const imgSetMarkup = createGalleryMarkup(galleryItems);
 
-const imgSetMarkup = createImgSetMarkup(galleryItems);
-imagesContainerRef.insertAdjacentHTML('beforeend', imgSetMarkup);
-
-function createImgSetMarkup(images) {
-  return images.map(({ preview, original, description }) => {
-    return `
-    <li class="gallery__item">
+function createGalleryMarkup(images) {
+  return images.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
@@ -27,68 +22,63 @@ function createImgSetMarkup(images) {
       alt="${description}"
     />
   </a>
-</li>
-    `;
+</li>`;
   }).join('');
 };
 
-imagesContainerRef.addEventListener('click', onImagesContainerClick);
-function onImagesContainerClick(e) {
+galleryRef.insertAdjacentHTML('beforeend', imgSetMarkup);
+
+galleryRef.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
   e.preventDefault();
-
-  const isTargetImgEl = e.target.classList.contains('gallery__image');
-  
-  if (!isTargetImgEl) {
-    return;
+  if (!e.target.classList.contains('gallery__image')) {
+    return; 
   };
-  
+
   lightboxRef.classList.add('is-open');
-  lightboxImageRef.src = e.target.dataset.source;
-  lightboxImageRef.alt = e.target.alt;
-  document.body.style.cssText += `height: 100%;
-   width: 100%;
-   position: fixed;
-   overflow: hidden;`
-  
-  window.addEventListener('keydown', onEscKeyPress);
-  window.addEventListener('keydown', onArrowKeyPress);
+  lightboxImagRef.src = e.target.dataset.source;
+  // lightboxImagRef.alt = e.target.alt;
+
+  document.body.style.cssText = `height: 100%; width: 100%; position: fixed; overflow: hidden;`
+
+  window.addEventListener('keydown', OnClsModalPressEsc);
+  window.addEventListener('keyup', onPressKeyArrow);
 };
 
-lightboxButtonRef.addEventListener('click', onCloseModalBtnClick);
-function onCloseModalBtnClick () {
+btnClsRef.addEventListener('click', onClsModalBtn);
+
+function onClsModalBtn() {
   lightboxRef.classList.remove('is-open');
-  lightboxImageRef.src = '';
-  lightboxImageRef.alt = '';
-  document.body.style.cssText -= `height: 100%;
-   width: 100%;
-   position: fixed;
-   overflow: hidden;`
+  lightboxImagRef.src = '';
   
-  window.removeEventListener('keydown', onEscKeyPress);
-  window.removeEventListener('keydown', onArrowKeyPress);
+  document.body.style.cssText -= `height: 100%; width: 100%; position: fixed; overflow: hidden;`
 
+  window.removeEventListener('keydown', OnClsModalPressEsc);
+  window.removeEventListener('keyup', onPressKeyArrow)
 };
 
-lightboxOverlayRef.addEventListener('click', onOverlayClick);
-function onOverlayClick(e) {
+overlayRef.addEventListener('click', OnClsModalPressKey);
+
+function OnClsModalPressKey(e) {
   if (e.target === e.currentTarget) {
-    onCloseModalBtnClick()
+    onClsModalBtn()
   };
 };
 
-function onEscKeyPress(e) {
+function OnClsModalPressEsc(e) {
   if (e.code === 'Escape') {
-  onCloseModalBtnClick();
+    onClsModalBtn()
   };
 };
 
 const arrImg = galleryItems.map(el => el.original);
-function onArrowKeyPress(e) {
-  const currentImg = arrImg.indexOf(lightboxImageRef.src);
-  console.log(lightboxImageRef.src)
-  if (e.code === 'ArrowRight' && currentImg < galleryItems.length - 1) {
-    lightboxImageRef.src = galleryItems[currentImg + 1].original;
-  } else if (e.code === 'ArrowLeft' && currentImg > 0) {
-    lightboxImageRef.src = galleryItems[currentImg - 1].original;
+
+function onPressKeyArrow(e) {
+  const currentImgIdx = arrImg.indexOf(lightboxImagRef.src);
+  if (e.code === 'ArrowRight' && currentImgIdx < galleryItems.length - 1) {
+    lightboxImagRef.src = galleryItems[currentImgIdx + 1].original;
+  } else if (e.code === 'ArrowLeft' && currentImgIdx > 0) {
+    lightboxImagRef.src = galleryItems[currentImgIdx - 1].original;
   };
 };
